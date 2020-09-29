e12268701f9324e61be7a341827fa336c3e9794e
Jean Bauer <jabauer@uchicago.edu>
2020-09-29 09:13:45 -0400

    Added modules for places, people, organizations, and credit (assigning researchers to the records they create). Also includes most of the first pass at the models for the inventory module.

--- app/inventory/models.py
+++ app/inventory/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class TGM_Genres(models.Model):
+	name = models.Charfield(max_lenght=50)
+	notes = models.Textfield(blank=True, null=True)
+
+class Original_Sources(models.model)
+	name = models.Charfield(max_lenght=765)
+	notes = models.Textfield(blank=True, null=True)
+
+class Items(models.Model):
+	#Choice lists used in fields below
+	DIGITIZATION_CHOICES = ['yes','no','maybe']
+	RECORD_STATUS_CHOICES = ['complete','incomplete','review','reviewed']
+	IONA_HOLDINGS_CHOICES = ['original', 'copy','original and copy',]
+
+	accession_number = models.Charfield(max_lenght=100, blank=True, null=True)
+	short_title = models.Charfield(max_lenght=200)
+	title = models.Textfield
+	size = models.Charfield(max_length=100, blank=True, null=True)
+	physical_description = models.Textfield(blank=True, null=True)
+	tgm_genre = models.ForeignKey(TGM_Genres, blank=True, null=True)
+	other_description = models.Textfield(blank=True, null=True)
+	condition_notes = model.Charfield(max_lenght=765, blank=True, null=True)
+	edition = model.Charfield(max_lenght=200, blank=True, null=True)
+	original_source = models.ForeignKey(Original_Sources, blank=True, null=True)
+	digitization_recommendation = model.Charfield(choices=DIGITIZATION_CHOICES, blank=True, null=True)
+	volume = models.Charfield(max_lenght=50, blank=True, null=True)
+	number = models.Charfield(max_lenght=50, blank=True, null=True)
+	record_status = models.Charfield(choices=RECORD_STATUS_CHOICES)
+	iona_holdings = models.Charfield(choices=IONA_HOLDINGS_CHOICES, blank=True, null=True)
+	notes = models.Textfield(blank=True, null=True)
