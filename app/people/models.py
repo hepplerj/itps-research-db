0054cdeb46399c312a7801cc1050c16e44b6029e
Jean Bauer <jabauer@uchicago.edu>
2021-01-27 16:34:45 -0500

    Added prosopography fields to Person model and created initial data import for dev and prod systems

--- app/people/models.py
+++ app/people/models.py
@@ -1,10 +1,25 @@
 from django.db import models
-
-# this is a stub record, based off the original inventory
+from django.contrib.postgres.fields import DateRangeField
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=765)
+    #this is awkward, use the notes field!
+    GENDER_CHOICES = [
+        ('man','man'),
+        ('woman','woman'),
+        ('non-binary','non-binary'),
+        ('other','other - describe in notes field'),
+        ('unknown','unknown')
+    ]
+
+    name = models.CharField(max_length=100)
+    display_name = models.CharField(max_length=100, blank=True, null=True)
+    birth_date = DateRangeField(blank=True, null=True)
+    death_date = DateRangeField(blank=True, null=True)
+    gender = models.CharField(max_length=15, choices=GENDER_CHOICES, blank=True, null=True)
+    viaf = models.CharField(max_length=100, blank=True, null=True)
+    wikipedia = models.CharField(max_length=100, blank=True, null=True)
+    getty = models.CharField(max_length=100, blank=True, null=True)
     notes = models.TextField(blank=True, null=True)
 
     def __str__(self):
