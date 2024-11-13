b713022a6aadea5c037343a4d96f6b8c18094077
Jason Heppler <jason.heppler@gmail.com>
2024-11-13 10:36:48 -0600

    refactor: Include new date fields for Item model

--- app/people/models.py
+++ app/people/models.py
@@ -1,23 +1,26 @@
-from django.db import models
 from django.contrib.postgres.fields import DateRangeField
+from django.db import models
+
 from places.models import State
 
 
 class Person(models.Model):
     # this is awkward, use the notes field!
     GENDER_CHOICES = [
-        ('man', 'man'),
-        ('woman', 'woman'),
-        ('non-binary', 'non-binary'),
-        ('other', 'other - describe in notes field'),
-        ('unknown', 'unknown')
+        ("man", "man"),
+        ("woman", "woman"),
+        ("non-binary", "non-binary"),
+        ("other", "other - describe in notes field"),
+        ("unknown", "unknown"),
     ]
 
-    name = models.CharField(max_length=100)
-    display_name = models.CharField(max_length=100, blank=True, null=True)
+    name = models.CharField(max_length=255)
+    display_name = models.CharField(max_length=255, blank=True, null=True)
     birth_date = DateRangeField(blank=True, null=True)
     death_date = DateRangeField(blank=True, null=True)
-    gender = models.CharField(max_length=15, choices=GENDER_CHOICES, blank=True, null=True)
+    gender = models.CharField(
+        max_length=15, choices=GENDER_CHOICES, blank=True, null=True
+    )
     home_state = models.ForeignKey(State, models.SET_NULL, blank=True, null=True)
     viaf = models.URLField(max_length=100, blank=True, null=True)
     wikipedia = models.URLField(max_length=100, blank=True, null=True)
