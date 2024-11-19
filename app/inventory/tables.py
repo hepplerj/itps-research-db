6e90c06282fcc483116238bb9b99d0bc6a1f879b
Jason Heppler <jason.heppler@gmail.com>
2024-11-19 09:33:42 -0600

    refactor: Remove AlpineJS integration, linkify the title

--- app/inventory/tables.py
+++ app/inventory/tables.py
@@ -6,38 +6,36 @@ from .models import Item
 
 
 class ItemTable(tables.Table):
-    accession_number = tables.Column(
-        linkify=lambda record: reverse("inventory:item_detail", args=[record.pk])
+    title = tables.Column(
+        linkify=lambda record: reverse("inventory:item_detail", args=[record.pk]),
+        attrs={
+            "a": {
+                "class": "text-iona-maroon hover:text-iona-aqua-green transition-colors"
+            }
+        },
+        verbose_name="Title",
     )
-    title = tables.Column()
-    tgm_genre = tables.Column()
-    pub_date = tables.Column()
-    record_status = tables.Column()
-    actions = tables.TemplateColumn(
-        template_code="""
-            <button type="button" 
-                    class="btn btn-sm btn-primary" 
-                    data-bs-toggle="modal" 
-                    data-bs-target="#itemModal" 
-                    data-item-id="{{ record.id }}">
-                View Details
-            </button>
-        """,
-        orderable=False,
-    )
+    tgm_genre = tables.Column(verbose_name="Genre")
+    pub_date = tables.Column(verbose_name="Date Range")
+    record_status = tables.Column(verbose_name="Status")
 
     class Meta:
         model = Item
-        template_name = "django_tables2/bootstrap5.html"
         fields = (
-            "accession_number",
-            "short_title",
+            "title",
             "tgm_genre",
             "pub_date",
             "record_status",
-            "actions",
         )
-        attrs = {"class": "table table-striped table-hover"}
+        attrs = {
+            "class": "table-fixed min-w-full divide-y divide-gray-300",
+            "th": {
+                "class": "px-3 py-3.5 text-left text-sm font-semibold text-gray-900 bg-gray-50"
+            },
+            "td": {"class": "whitespace-nowrap px-3 py-4 text-sm text-gray-500"},
+            "tr": {"class": "even:bg-gray-50"},
+        }
+        pagination_template = "django_tables2/pagination.html"
 
 
 class ItemFilter(django_filters.FilterSet):
